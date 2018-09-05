6b36b6871fae200e5b71ccf32894142c59c540f0
Sergio Roman <sergio@ricoma.us>
2018-09-05 02:30:37 -0400

    trash

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
+var stripe = require("stripe")("sk_test_x6IdvtCY7o6gtBc3Txie41YE");
 
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
 
 var port = process.env.PORT || 5000;
 app.listen(port);
+
+
+app.post('/process-payment', function (req, res) {
+    const token = req.data.stripeToken; // Using Express
+
+    const charge = stripe.charges.create({
+        amount: 49.95,
+        currency: 'usd',
+        description: 'Animated Logo - Promotion',
+        source: token,
+    });
+});
