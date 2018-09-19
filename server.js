9db50c05fa61f2c92bc5c5d13768778e06c4f21b
Sergio Roman <sergio@ricoma.us>
2018-09-19 01:36:32 -0400

    Revert "Merge branch 'serge'"
    This reverts commit 9ef9a5818a3a5343cdb7dc58deef54c16bcec77a, reversing
    changes made to 354b02c2fbd74106bbb8d4cad87a9029544e074d.

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ app.use(require("body-parser").urlencoded({extended: false}));
 
 var port = process.env.PORT || 5000;
 
-
 app.post('/charge', function (req, res) {
     let amount = 19999;
     res.setHeader('Content-Type', 'application/json');
@@ -29,7 +28,7 @@ app.post('/charge', function (req, res) {
         }));
 });
 
-app.post('/charge/sale', function (req, res) {
+app.post('/sale', function (req, res) {
     let amount = 4999;
     res.setHeader('Content-Type', 'application/json');
 
