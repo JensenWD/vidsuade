a79f26a9faac8d428faff6ef3a7f6157d97651a5
Johnatan Jensen <jensenwd3@gmail.com>
2018-11-15 03:24:47 -0500

    edits

--- src/components/Landing Page/LandingPage3.vue
+++ src/components/Landing Page/LandingPage3.vue
@@ -57,14 +57,86 @@
 
     <div class="row text-center position-relative light-blue-bg pb-5 pt-5">
       <div class="container pb-5 pt-5">
-        <h2 class="mt-2 mb-5 font-weight-light font-white">Need a video for your app?</h2>
+        <h2 class="mt-2 mb-2 font-weight-light font-white">Need a video for your app?</h2>
+        <button class="btn white-btn">Contact us</button>
       </div>
     </div>
 
     <div class="row text-center position-relative pb-5 pt-5">
       <img class="img-fluid overlap2" src="../../assets/landing_page3/App_Landing_Reviews_Overlap_03_Shadow.png" alt="">
       <div class="container pb-5 pt-5">
-        <h2 class="mt-2 mb-5 font-weight-light font-white">Benefits of app videos:</h2>
+        <div class="row">
+          <div class="col-12">
+            <h2 class="mt-2 mb-5 font-weight-light font-orange">Benefits of app videos:</h2>
+            <ul class="items-checklist p-0 col-10 offset-1 col-xl-6 offset-xl-3 text-left">
+              <li><img class="img-fluid" src="../../assets/landing_page3/App_Landing_Check_Mark.png" alt=""><span>Get more app installs</span></li>
+              <li><img class="img-fluid" src="../../assets/landing_page3/App_Landing_Check_Mark.png" alt=""><span>Increase your conversions</span></li>
+              <li><img class="img-fluid" src="../../assets/landing_page3/App_Landing_Check_Mark.png" alt=""><span>Clearly show how your app works</span></li>
+              <li><img class="img-fluid" src="../../assets/landing_page3/App_Landing_Check_Mark.png" alt=""><span>Creates excitement and demand</span></li>
+              <li><img class="img-fluid" src="../../assets/landing_page3/App_Landing_Check_Mark.png" alt=""><span>Have a professional looking ad</span></li>
+              <li><img class="img-fluid" src="../../assets/landing_page3/App_Landing_Check_Mark.png" alt=""><span>Highly shareable</span></li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="row mt-3 mb-3 price-section">
+          <div class="col-12 col-md-10 offset-md-1 top-break pb-5 pt-5 text-center">
+            <h2 class="mt-2 mb-5 font-weight-light font-orange">Types of app videos</h2>
+            <div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><div class="wistia_embed wistia_async_1gsd7phy3v videoFoam=true" style="height:100%;position:relative;width:100%"><div class="wistia_swatch" style="height:100%;left:0;opacity:0;overflow:hidden;position:absolute;top:0;transition:opacity 200ms;width:100%;"><img src="https://fast.wistia.com/embed/medias/1gsd7phy3v/swatch" style="filter:blur(5px);height:100%;object-fit:contain;width:100%;" alt="" onload="this.parentNode.style.opacity=1;" /></div></div></div></div>
+            <div class="sub_header"><span>APP PROMOS</span></div>
+            <p class="pt-2 pb-4">With app promos people will know exactly what the app does and what it
+              looks like so people can make a decision about whether they want to
+              download your mobile app or not. App promo videos are GREAT for
+              managing user expectations.</p>
+            <div class="pricing pt-3">
+              <span class="font-blue">Starting at only $499</span>
+              <button class="btn orange-btn font-white ml-4">GET MY FREE QUOTE</button>
+            </div>
+          </div>
+        </div>
+
+        <div class="row mt-3 mb-3 price-section">
+          <div class="col-12 col-md-10 offset-md-1 top-break pb-5 pt-5 text-center">
+            <div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><div class="wistia_embed wistia_async_1gsd7phy3v videoFoam=true" style="height:100%;position:relative;width:100%"><div class="wistia_swatch" style="height:100%;left:0;opacity:0;overflow:hidden;position:absolute;top:0;transition:opacity 200ms;width:100%;"><img src="https://fast.wistia.com/embed/medias/1gsd7phy3v/swatch" style="filter:blur(5px);height:100%;object-fit:contain;width:100%;" alt="" onload="this.parentNode.style.opacity=1;" /></div></div></div></div>
+            <div class="sub_header"><span>ANIMATED EXPLAINER VIDEOS</span></div>
+            <p class="pt-2">Animated videos are great for attracting attention and increasing
+              awareness of your brand. They’re one of the most shareable forms of
+              video, and this all leads towards the same end goal: increasing your mobile
+              app downloads.</p>
+            <p class="pb-4">An animated explainer video is an Illustrated animation representation of
+              your app that shows people what your mobile app is about and how it can
+              help them. It helps connect with your audience on a more emotional level
+              and clearly explains what it does in a fun way.</p>
+            <div class="pricing pt-3">
+              <span class="font-blue">Starting at only $1499</span>
+              <button class="btn orange-btn font-white ml-4">GET MY FREE QUOTE</button>
+            </div>
+          </div>
+        </div>
+
+        <div class="row mt-3 mb-3 price-section">
+          <div class="col-12 col-md-10 offset-md-1 top-break pb-5 pt-5 text-center">
+            <div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><div class="wistia_embed wistia_async_1gsd7phy3v videoFoam=true" style="height:100%;position:relative;width:100%"><div class="wistia_swatch" style="height:100%;left:0;opacity:0;overflow:hidden;position:absolute;top:0;transition:opacity 200ms;width:100%;"><img src="https://fast.wistia.com/embed/medias/1gsd7phy3v/swatch" style="filter:blur(5px);height:100%;object-fit:contain;width:100%;" alt="" onload="this.parentNode.style.opacity=1;" /></div></div></div></div>
+            <div class="sub_header"><span>APP PREVIEWS</span></div>
+            <p class="pt-2 pb-4">A video preview of your app is the absolute best way to demostrate to
+              users what your app does and will help you to manage customer
+              expectations…while also giving you a competitive edge. It can grab the
+              attention of thousands and at the same time, it can give a better insight of
+              your app.</p>
+            <div class="pricing pt-3">
+              <span class="font-blue">Starting at only $249</span>
+              <button class="btn orange-btn font-white ml-4">GET MY FREE QUOTE</button>
+            </div>
+          </div>
+        </div>
+
+        <div class="row text-center position-relative light-grey-bg pb-5 pt-5">
+          <div class="container pb-5 pt-5">
+            <h2 class="mt-2 mb-2 font-weight-light font-white">Need a video for your app?</h2>
+            <button class="btn white-btn">Contact us</button>
+          </div>
+        </div>
+
       </div>
     </div>
 
@@ -73,11 +145,63 @@
 
 <script>
   export default {
-    name: "LandingPage3"
+    name: "LandingPage3",
+    mounted() {
+      let whyVideo = document.createElement('script');
+      whyVideo.setAttribute('src', 'https://fast.wistia.com/embed/medias/1gsd7phy3v.jsonp');
+      document.head.appendChild(whyVideo);
+    }
   }
 </script>
 
 <style lang="sass" scoped>
+  p
+    font-size: 21px
+
+  .price-section
+    p
+      text-align: left
+
+  .orange-btn
+    background: #E98000
+    border-radius: 8px
+    box-shadow: 0 0 7px #9c9c9c
+    padding: 0.5rem 0.8rem
+    border: none
+    font-size: 24px
+    font-weight: 700
+
+  .pricing
+    display: flex
+    align-items: center
+    justify-content: center
+    span
+      font-size: 36px
+      font-weight: 700
+
+  .sub_header
+    padding: 1.5rem 0 1.2rem
+    span
+      font-size: 36px
+      text-transform: uppercase
+      font-weight: 700
+
+  .top-break
+    border-top: 1px solid #9e9e9e
+
+  .items-checklist
+    list-style: none
+    li
+      margin-bottom: 1.5rem
+      display: flex
+      align-items: center
+      justify-content: flex-start
+      img
+        margin-right: 0.5rem
+      span
+        font-size: 28px
+        font-weight: 700
+
   .light-blue-bg
     background: #00a9e6
     box-shadow: 0 3px 6px rgba(0, 0, 0, 0.2)
